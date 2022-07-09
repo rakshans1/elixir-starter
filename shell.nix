b00af5b61e54dd3ad395433830a8d201b08b5f93
Rakshan Shetty <shetty.raxx555@gmail.com>
2022-07-09 13:25:05 +0530

    chore: setup shell hooks

--- shell.nix
+++ shell.nix
@@ -5,18 +5,36 @@
 with pkgs;
 let
   inherit (lib) optional optionals;
+
+  basePackages = [
+    (import ./nix/default.nix { inherit pkgs; })
+    pkgs.niv
+  ];
+
+  inputs =  basePackages ++ lib.optionals stdenv.isLinux [ inotify-tools ]
+    ++ lib.optionals stdenv.isDarwin
+    (with darwin.apple_sdk.frameworks; [ CoreFoundation CoreServices ]);
+
+  hooks = ''
+    # this allows mix to work on the local directory
+    mkdir -p .nix-mix .nix-hex
+    export MIX_HOME=$PWD/.nix-mix
+    export HEX_HOME=$PWD/.nix-hex
+    # make hex from Nixpkgs available
+    # `mix local.hex` will install hex into MIX_HOME and should take precedence
+    export MIX_PATH="${beam.packages.erlangR24.hex}/lib/erlang/lib/hex/ebin"
+    export PATH=$MIX_HOME/bin:$HEX_HOME/bin:$PATH
+    export LANG=C.UTF-8
+    # keep your shell history in iex
+    export ERL_AFLAGS="-kernel shell_history enabled"
+
+    export MIX_ENV=dev
+  '';
 in
 
 mkShell {
-  buildInputs = [
-    (import ./nix/default.nix { inherit pkgs; })
-    niv
-  ] ++ optional stdenv.isLinux inotify-tools
-  ++ optional stdenv.isDarwin terminal-notifier
-  ++ optionals stdenv.isDarwin (with darwin.apple_sdk.frameworks; [
-    CoreFoundation
-    CoreServices
-  ]);
+  buildInputs = inputs;
+  shellHook = hooks;
 
   LOCALE_ARCHIVE = if pkgs.stdenv.isLinux then "${pkgs.glibcLocales}/lib/locale/locale-archive" else "";
 }
