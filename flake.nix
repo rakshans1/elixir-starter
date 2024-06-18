f647ef9ae2a1ddb5bccabcc35aa434e308f3da2d
Rakshan Shetty <shetty.raxx555@gmail.com>
2024-06-18 14:26:23 +0530

    chore: update flake

--- flake.nix
+++ flake.nix
@@ -29,7 +29,7 @@
         devShells = {
           default = pkgs.mkShell {
             packages = with pkgs;  [
-              beam.packages.erlangR26.elixir_1_16
+              beam.packages.erlang_26.elixir_1_16
             ] ++ linuxDeps ++ darwinDeps;
             shellHook = ''
               # this allows mix to work on the local directory
@@ -38,7 +38,7 @@
               export HEX_HOME=$PWD/.nix-hex
               # make hex from Nixpkgs available
               # `mix local.hex` will install hex into MIX_HOME and should take precedence
-              export MIX_PATH="${pkgs.beam.packages.erlangR26.hex}/lib/erlang/lib/hex/ebin"
+              export MIX_PATH="${pkgs.beam.packages.erlang_26.hex}/lib/erlang/lib/hex/ebin"
               export PATH=$MIX_HOME/bin:$HEX_HOME/bin:$PATH
               export LANG=C.UTF-8
               # keep your shell history in iex
