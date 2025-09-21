6f46542caf06c08c7b706b7f7e7340aad7815028
Rakshan Shetty <shetty.raxx555@gmail.com>
2025-09-21 14:14:28 +0530

    chore: update flake

--- flake.nix
+++ flake.nix
@@ -6,43 +6,43 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-        };
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
 
-        inherit (pkgs) inotify-tools terminal-notifier;
-        inherit (pkgs.lib) optionals;
-        inherit (pkgs.stdenv) isDarwin isLinux;
+      inherit (pkgs) inotify-tools terminal-notifier;
+      inherit (pkgs.lib) optionals;
+      inherit (pkgs.stdenv) isDarwin isLinux;
 
-        linuxDeps = optionals isLinux [ inotify-tools ];
-        darwinDeps = optionals isDarwin [ terminal-notifier ];
-
-      in
-      {
-        devShells = {
-          default = pkgs.mkShell {
-            packages = with pkgs;  [
+      linuxDeps = optionals isLinux [inotify-tools];
+      darwinDeps = optionals isDarwin [terminal-notifier];
+    in {
+      devShells = {
+        default = pkgs.mkShell {
+          packages = with pkgs;
+            [
               beam.packages.erlang_27.elixir_1_18
-            ] ++ linuxDeps ++ darwinDeps;
-            shellHook = ''
-              # this allows mix to work on the local directory
-              mkdir -p .nix-mix .nix-hex
-              export MIX_HOME=$PWD/.nix-mix
-              export HEX_HOME=$PWD/.nix-hex
-              # make hex from Nixpkgs available
-              # `mix local.hex` will install hex into MIX_HOME and should take precedence
-              export MIX_PATH="${pkgs.beam.packages.erlang_27.hex}/lib/erlang/lib/hex/ebin"
-              export PATH=$MIX_HOME/bin:$HEX_HOME/bin:$PATH
-              export LANG=C.UTF-8
-              # keep your shell history in iex
-              export ERL_AFLAGS="-kernel shell_history enabled"
+            ]
+            ++ linuxDeps ++ darwinDeps;
+          shellHook = ''
+            mkdir -p .nix-mix .nix-hex
+            export MIX_HOME=$PWD/.nix-mix
+            export HEX_HOME=$PWD/.nix-hex
+            export MIX_PATH="${pkgs.beam.packages.erlang_27.hex}/lib/erlang/lib/hex/ebin"
+            export PATH=$MIX_HOME/bin:$HEX_HOME/bin:$PATH
+            export LANG=C.UTF-8
+            # keep your shell history in iex
+            export ERL_AFLAGS="-kernel shell_history enabled"
 
-              export MIX_ENV=dev
-            '';
-          };
+            export MIX_ENV=dev
+          '';
         };
-      });
+      };
+    });
 }
